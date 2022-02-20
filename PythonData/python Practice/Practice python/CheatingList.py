22fe2e09caf8258d62023b3c5c9ba35140587c7d
Aadesh <aadeshlokhande11@gmail.com>
2022-02-20 19:26:48 +0530

    List

---
+++ PythonData/python Practice/Practice python/CheatingList.py
@@ -0,0 +1,22 @@
+list = [
+"Hemant vaishnav",
+"BHAVESH Khanorkar",
+"Aashitosh Gajbhiye",
+"Ashish Kadwe",
+"Abhishek Wadichar",
+"Premraj Sakure",
+"Prajwal wadichar",
+"Chandrabhan uikey ",
+"Pratik kshirsagar",
+"Piyush nimkar",
+"Deepak thakre",
+"Swapnil Shambharkar",
+"Harshal shende"]
+count = 1
+list.sort()
+for i in range(len(list)):
+    print(list[i])
+    for j in range(3):
+        print(f"Question No. {count}  ==  ")
+        count+=1
+    print()
