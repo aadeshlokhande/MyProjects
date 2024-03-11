8b789b13e8de9c37a048f84fbdce055e610f0037
Aadesh <aadeshlokhande11@gmail.com>
2024-03-11 20:12:54 +0530

    11 / 03 / 2024

--- Instagram/Tech_in_seconds/asd.py
+++ Instagram/Tech_in_seconds/asd.py
@@ -1,12 +1,1 @@
-# hollow square
 
-# try this.... remove all underscore(_) before my code
-
-size = 5
-for i in range(size):
-____for j in range(size):
-________if(i==0 or j==0 or i==size-1 or j==size-1):
-____________print("*",end=" ")
-________else:
-____________print(" ", end=" ")
-____print()
